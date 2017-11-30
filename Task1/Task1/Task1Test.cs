9dc7c44417d344f1a98fe4e2bda1f11923fa6ea9
Dmitry Klementyev <dmitriklementev@gmail.com>
2017-12-01 02:41:48 +0300

    Test Commit

---
+++ Task1/Task1/Task1Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+namespace Task1
+{
+    [TestClass()]
+    public class Task1Test
+    {
+        #region Constants
+        private const int WAIT_TIMEOUT = 10;
+        private const string URL = "https://google.ru/";
+        private const string EXPECTED_TITLE = "Chrome";
+        #endregion
+
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        
+        [TestInitialize()]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(WAIT_TIMEOUT));
+        }
+        [TestMethod()]
+        public void TestCase()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(WAIT_TIMEOUT);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(URL);
+            wait.Until(ExpectedConditions.TitleIs(EXPECTED_TITLE));
+        }
+
+        [TestCleanup()]
+        public void TestCleanUp()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
