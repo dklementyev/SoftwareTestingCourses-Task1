e5a9b63803bd6cc5ff0df80157cd68458867558e
Dmitry Klementyev <dmitriklementev@gmail.com>
2018-01-09 23:46:23 +0300

    add Task11 And Task12

---
+++ Task1/Task1/Task11.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace Task1
+{
+    [TestClass]
+    public class Task11
+    {
+        private const string ADMIN_LOGIN = "admin";
+        private const string ADMIN_PASSWORD = "admin";
+        private const string URL = "http://localhost/litecart";
+        private const int WAIT_TIMEOUT = 10;
+        private const string EXPECTED_TITLE = "Online Store | My Store";
+        private const string BROWSER = "Chrome";
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+
+
+        private void BrowserSetup()
+        {
+            if (BROWSER == "Chrome")
+            {
+                driver = new ChromeDriver();
+                return;
+            }
+            if (BROWSER == "Firefox")
+            {
+                driver = new FirefoxDriver();
+                return;
+            }
+            if (BROWSER == "IE")
+            {
+                driver = new InternetExplorerDriver();
+                return;
+            }
+
+            Assert.Fail("Browser does not supported");
+        }
+        [TestInitialize()]
+        public void Task11_Setup()
+        {
+            BrowserSetup();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(WAIT_TIMEOUT));
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(URL);
+            wait.Until(ExpectedConditions.TitleIs(EXPECTED_TITLE));
+        }
+
+        [TestMethod]
+        public void Task11_Case_CreateCustomer()
+        {
+            //Click on New Customer Link
+            driver.FindElement(By.XPath(".//a[contains(text(),'New customers')]")).Click();
+
+            //Fill new customer fields
+            var firstNameField = driver.FindElement(By.XPath(".//input[@name='firstname']"));
+            var lastNameField = driver.FindElement(By.XPath(".//input[@name='lastname']"));
+            var address1Field = driver.FindElement(By.XPath(".//input[@name='address1']"));
+            var postCodeField = driver.FindElement(By.XPath(".//input[@name='postcode']"));
+            var cityField = driver.FindElement(By.XPath(".//input[@name='city']"));
+            var countrySelectElement = driver.FindElement(By.XPath(".//select[@name='country_code']"));
+            var emailField = driver.FindElement(By.XPath(".//input[@name='email']"));
+            var phoneField = driver.FindElement(By.XPath(".//input[@name='phone']"));
+            var passwordField = driver.FindElement(By.XPath(".//input[@name='password']"));
+            var confirmPasswordField = driver.FindElement(By.XPath(".//input[@name='confirmed_password']"));
+            var createAccountButton = driver.FindElement(By.XPath(".//button[@name='create_account']"));
+
+            firstNameField.Clear();
+            firstNameField.SendKeys("Test FName");
+
+            lastNameField.Clear();
+            lastNameField.SendKeys("Test LName");
+
+            address1Field.Clear();
+            address1Field.SendKeys("Test Address1");
+
+            postCodeField.Clear();
+            postCodeField.SendKeys("55555");
+
+            cityField.Clear();
+            cityField.SendKeys("Test City");
+
+            var countrySelector = new SelectElement(countrySelectElement);
+            countrySelector.SelectByValue("US");
+
+            var email = String.Format("TestEmail{0}@email.com", Guid.NewGuid());
+            emailField.Clear();
+            emailField.SendKeys(email);
+
+            phoneField.SendKeys("+15555555555");
+
+            var password = "TestPassword";
+            passwordField.Clear();
+            passwordField.SendKeys(password);
+
+            confirmPasswordField.Clear();
+            confirmPasswordField.SendKeys(password);
+
+
+            //Click on create account button
+            createAccountButton.Click();
+
+             
+            //Logout
+            var logoutLink = driver.FindElement(By.XPath(".//a[text()='Logout']"));
+            logoutLink.Click();
+
+            //LogIn
+            var loginField = driver.FindElement(By.XPath(".//input[@name='email']"));
+            var pwdField = driver.FindElement(By.XPath(".//input[@name='password']"));
+            var loginButton = driver.FindElement(By.XPath(".//button[@name='login']"));
+
+            loginField.Clear();
+            loginField.SendKeys(email);
+
+            pwdField.Clear();
+            pwdField.SendKeys(password);
+
+            loginButton.Click();
+
+            //Logout again
+            logoutLink = driver.FindElement(By.XPath(".//a[text()='Logout']"));
+            logoutLink.Click();
+
+        }
+
+        [TestCleanup()]
+        public void Task11_TearDown()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ Task1/Task1/Task12.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace Task1
+{
+    [TestClass]
+    public class Task12
+    {
+        private const string ADMIN_LOGIN = "admin";
+        private const string ADMIN_PASSWORD = "admin";
+        private const string URL = "http://localhost/litecart/admin";
+        private const int WAIT_TIMEOUT = 10;
+        private const string EXPECTED_TITLE = "My Store";
+        private const string BROWSER = "Chrome";
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private string PRODUCT_GROUP = "Unisex";
+
+
+        private void BrowserSetup()
+        {
+            if (BROWSER == "Chrome")
+            {
+                driver = new ChromeDriver();
+                return;
+            }
+            if (BROWSER == "Firefox")
+            {
+                driver = new FirefoxDriver();
+                return;
+            }
+            if (BROWSER == "IE")
+            {
+                driver = new InternetExplorerDriver();
+                return;
+            }
+
+            Assert.Fail("Browser does not supported");
+        }
+        [TestInitialize()]
+        public void Task12_Setup()
+        {
+            BrowserSetup();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(WAIT_TIMEOUT));
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(URL);
+            wait.Until(ExpectedConditions.TitleIs(EXPECTED_TITLE));
+        }
+
+        [TestMethod]
+        public void Task12_Case_CreateCustomer()
+        {
+            //Login
+
+            wait.Until(ExpectedConditions.TitleIs(EXPECTED_TITLE));
+            var loginField = this.driver.FindElement(By.XPath(".//input[@name='username']"));
+            var pwdField = this.driver.FindElement(By.XPath(".//input[@name='password']"));
+            var loginBtn = this.driver.FindElement(By.XPath(".//button[@name='login']"));
+
+            loginField.Clear();
+            loginField.SendKeys(ADMIN_LOGIN);
+
+            pwdField.Clear();
+            pwdField.SendKeys(ADMIN_PASSWORD);
+
+            loginBtn.Click();
+
+            driver.FindElement(By.XPath(".//li[contains(@id,'app-')]/a//span[contains(text(),'Catalog')]")).Click();
+
+            driver.FindElement(By.XPath(".//a[contains(text(),'Add New Product')]")).Click();
+
+            driver.FindElement(By.XPath(".//input[@name='status' and @value='1']")).Click();
+
+            var nameField = driver.FindElement(By.XPath(".//input[contains(@name,'name')]"));
+            var codeField = driver.FindElement(By.XPath(".//input[contains(@name,'code')]"));
+            driver.FindElement(By.XPath($".//td[text()='{PRODUCT_GROUP}']/../td[1]")).Click();
+            var quantityField = driver.FindElement(By.XPath(".//input[@name='quantity']"));
+            var quantityUnitSelect = driver.FindElement(By.XPath(".//select[@name='quantity_unit_id']"));
+            var deliveryStatusSelect = driver.FindElement(By.XPath(".//select[@name='delivery_status_id']"));
+            var soldOutStatusSelect = driver.FindElement(By.XPath(".//select[@name='sold_out_status_id']"));
+            var dateValidFrom = driver.FindElement(By.XPath(".//input[@name='date_valid_from']"));
+            var dateValidTo = driver.FindElement(By.XPath(".//input[@name='date_valid_to']"));
+
+            var imagePath = Directory.GetCurrentDirectory() + @"\images\productImage.jpg";
+            var productImageUpload = driver.FindElement(By.XPath(".//input[@name='new_images[]']"));
+
+
+            var productName = $"Test Product {Guid.NewGuid()}";
+            nameField.Clear();
+            nameField.SendKeys(productName);
+
+            var productCode = $"Test Code {Guid.NewGuid()}";
+            codeField.Clear();
+            codeField.SendKeys(productCode);
+
+            quantityField.Clear();
+            quantityField.SendKeys("1");
+
+            var quantityUnitSelector = new SelectElement(quantityUnitSelect);
+            quantityUnitSelector.SelectByText("pcs");
+
+            var deliveryStatusSelector = new SelectElement(deliveryStatusSelect);
+            deliveryStatusSelector.SelectByText("3-5 days");
+
+            var soldOutStatusSelector = new SelectElement(soldOutStatusSelect);
+            soldOutStatusSelector.SelectByText("Sold out");
+
+            productImageUpload.SendKeys(imagePath);
+
+            dateValidFrom.SendKeys("01.01.1970");
+
+            dateValidTo.SendKeys("31.12.2018");
+
+            var informationTab = driver.FindElement(By.XPath(".//a[text()='Information']"));
+
+            informationTab.Click();
+
+            System.Threading.Thread.Sleep(5000);
+
+            var manufacturerSelect = driver.FindElement(By.XPath(".//select[@name='manufacturer_id']"));
+            //var supplierSelect = driver.FindElement(By.XPath(".//select[@name='supplier_id']"));
+            var keywordsField = driver.FindElement(By.XPath(".//input[@name='keywords']"));
+            var shortDescription = driver.FindElement(By.XPath(".//input[contains(@name,'short_description')]"));
+            var descriptionField = driver.FindElement(By.XPath(".//div[contains(@class,'trumbowyg-editor')]"));
+            var headTitleField = driver.FindElement(By.XPath(".//input[contains(@name,'head_title')]"));
+            var metaDescriptionField = driver.FindElement(By.XPath(".//input[contains(@name,'meta_description')]"));
+            
+            var manufacturerSelector = new SelectElement(manufacturerSelect);
+            manufacturerSelector.SelectByText("ACME Corp.");
+
+            keywordsField.Clear();
+            keywordsField.SendKeys("KeyWords");
+            
+            shortDescription.Clear();
+            shortDescription.SendKeys("Short Description");
+
+            new Actions(driver).SendKeys(descriptionField,"Description").Perform();
+
+            headTitleField.Clear();
+            headTitleField.SendKeys("HeadTitle");
+
+            metaDescriptionField.Clear();
+            metaDescriptionField.SendKeys("Meta Description");
+
+
+            var pricesTab = driver.FindElement(By.XPath(".//a[text()='Prices']"));
+            pricesTab.Click();
+
+            var purchasePriceField = driver.FindElement(By.XPath(".//input[@name='purchase_price']"));
+            var currencySelect = driver.FindElement(By.XPath(".//select[@name='purchase_price_currency_code']"));
+            var saveButton = driver.FindElement(By.XPath(".//button[@name='save']"));
+
+            purchasePriceField.Clear();
+            purchasePriceField.SendKeys("1,00");
+            var currencySelector = new SelectElement(currencySelect);
+            currencySelector.SelectByValue("USD");
+
+            saveButton.Click();
+
+         }
+
+        [TestCleanup()]
+        public void Task12_TearDown()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
